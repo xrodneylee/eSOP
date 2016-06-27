3e8b8c9c4f13e1299917fd5b5def4da9c88bf8fc
GuanPu <xrodneylee@gmail.com>
2016-06-27 17:27:37 +0800

    環境架設

--- src/com/dci/esop/ajax/ajaxController.java
+++ src/com/dci/esop/ajax/ajaxController.java
@@ -1,17 +1,25 @@
 package com.dci.esop.ajax;
 
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import net.sf.json.JSONObject;
 
 @Path("/ajax")
 public class ajaxController {
+	@GET
+    @Produces("text/plain")
+    public String sayHelloWorld() {
+        return "Hello world";
+    }  
+	
 	@POST
-	@Path("/helloPost")
+	@Path("/hello")
 	public String sayHelloPost(@FormParam("data") String jsonObj) {
 		JSONObject jsonObject = JSONObject.fromObject(jsonObj);
-		return "Hello, " + jsonObject.getString("name");
+		return "Hello, " + jsonObject.getString("USER");
 	}
 }
